24ad1987c3841db75fc7a99d681646f7e89eae6e
DEV.HYUCK <wwh3218@gmail.com>
2025-12-31 15:37:25 +0900

    feat : new file ( User data Create 1 )

--- src/main/java/com/example/jpa/dto/UserCreateRequest.java
+++ src/main/java/com/example/jpa/dto/UserCreateRequest.java
@@ -5,4 +5,8 @@ import lombok.Getter;
 @Getter
 
 public class UserCreateRequest {
+
+    private String name;
+    private String email;
+    private String password;
 }
--- src/main/java/com/example/jpa/repository/UserRepository.java
+++ src/main/java/com/example/jpa/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.example.jpa.repository;
 
-public interface UserRepository {
+import com.example.jpa.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
 }
